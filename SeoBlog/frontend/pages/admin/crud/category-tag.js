4c66873b01dd23cb6a68cdcc87771f81708c0f89
Kevin Narsh <kpnaranj@uwaterloo.ca>
2020-10-18 02:02:10 -0400

    Development of crud categories and tags

---
+++ SeoBlog/frontend/pages/admin/crud/category-tag.js
@@ -0,0 +1,29 @@
+import Layout from '../../../components/Layout';
+import Admin from '../../../components/auth/Admin';
+import Link from 'next/link';
+
+
+const CategoryTag =()=> {
+    
+    return (
+        <Layout>
+            <Admin>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-12 pt-5 pb-5"> <h2>Manage Categories and tags</h2> </div>
+                        <div className="col-md-4">
+                            <p>category</p>
+                        </div>
+                        <div className="col-md-8">tag</div>
+                    </div>
+                </div>
+                
+             
+            </Admin>
+        </Layout>
+    )
+}
+
+
+
+export default CategoryTag;
--- SeoBlog/frontend/pages/admin/index.js
+++ SeoBlog/frontend/pages/admin/index.js
@@ -8,7 +8,24 @@ const AdminIndex =()=> {
     return (
         <Layout>
             <Admin>
-              <h2>Admin Dashboard</h2>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-12 pt-5 pb-5"> <h2>Admin Dashboard</h2> </div>
+                        <div className="col-md-4">
+                            <ul className="list-group">
+                                <li className="list-group-item">
+                                    <Link href="/admin/crud/category-tag">
+                                        <a>Create category</a>
+                                    </Link>
+                                </li>
+                            </ul>
+                            
+                        </div>
+                        <div className="col-md-8">rigth</div>
+                    </div>
+                </div>
+                
+             
             </Admin>
         </Layout>
     )
