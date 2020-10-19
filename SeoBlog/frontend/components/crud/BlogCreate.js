cb4e78474dd8cbcb550d7d18b8ba810da4d19a0c
Kevin Narsh <kpnaranj@uwaterloo.ca>
2020-10-19 03:25:51 -0400

    Complete delivery of tags

--- SeoBlog/frontend/components/crud/BlogCreate.js
+++ SeoBlog/frontend/components/crud/BlogCreate.js
@@ -5,6 +5,7 @@ import dynamic from 'next/dynamic'; //React will run on client
 import {withRouter} from 'next/router';
 import {getCookie, isAuth} from '../../actions/auth';
 import {getCategories} from '../../actions/category';
+import {getTags} from '../../actions/tag';
 import {CreateBlog} from '../../actions/blog';
 const ReactQuill = dynamic(()=>import('react-quill'),{ssr: false});
 import '../../node_modules/react-quill/dist/quill.snow.css';
@@ -24,7 +25,10 @@ const BlogCreate = ({router}) =>{
             return false;
         }
     }
-
+    
+    const [categories, setCategories] = useState([]);
+    const [tags, setTags] = useState([]);
+    
     const [body, setBody] = useState(blogFromLS());
     const [values, setValues] = useState({
         error: '',
@@ -38,8 +42,35 @@ const BlogCreate = ({router}) =>{
     const {error, sizeError, sucess, formData, title, hidePublishButton} = values;
     
     useEffect(()=>{
-        setValues({...values, formData: new FormData()})  
+        setValues({...values, formData: new FormData()});
+        initCategories();
+        initTags();
     },[router])
+    
+    const initCategories =()=>{
+        getCategories().then(data=>{
+            if(data.error){
+                setValues({...values, error: data.error})
+            }
+            
+            else{
+                setCategories(data);
+            }
+        })
+    }
+    
+    const initTags =()=>{
+        getTags().then(data=>{
+            if(data.error){
+                setValues({...values, error: data.error})
+            }
+            
+            else{
+                setTags(data);
+            }
+        })
+        
+    }
     
     const publishBlog =(e)=>{
         e.preventDefault();
@@ -58,6 +89,29 @@ const BlogCreate = ({router}) =>{
         if(typeof window !== 'undefined'){
             localStorage.setItem('blog', JSON.stringify(e))
         }
+    }
+    
+    const showCategories = () =>{
+        return (
+            categories && categories.map((category, index)=>(
+                <li key={index} className="list-unstyled">
+                    <input type="checkbox" className="mr-2"/>
+                    <label className="form-check-label">{category.name}</label>
+                </li>
+            ))
+        )
+    }
+    
+     
+    const showTags = () =>{
+        return (
+            tags && tags.map((tag, index)=>(
+                <li key={index} className="list-unstyled">
+                    <input type="checkbox" className="mr-2"/>
+                    <label className="form-check-label">{tag.name}</label>
+                </li>
+            ))
+        )
     }
     
     const createBlogForm =() =>{
@@ -82,10 +136,39 @@ const BlogCreate = ({router}) =>{
     }
 
     return (
-        <div>
-            {createBlogForm()}
-            <hr/>
-            {JSON.stringify(title)}
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-md-8">
+                    {createBlogForm()}
+                    <hr/>
+                    {JSON.stringify(title)}
+                    <hr/>
+                    {JSON.stringify(body)}
+                    <hr/>
+                    {JSON.stringify(categories)}
+                    <hr/>
+                    {JSON.stringify(tags)}
+                </div>
+                
+                <div className="col-md-4">
+                    <div>
+                        <h5>Categories</h5>
+                        <hr/>
+                        <ul style={{maxHeight: '200px', overflowYL: 'scroll'}}>
+                            {showCategories()}
+                        </ul>
+                        
+                    </div>
+                    <div>
+                        <h5>Tags</h5>
+                        <hr/>
+                        <ul style={{maxHeight: '200px', overflowYL: 'scroll'}}>
+                           {showTags()} 
+                        </ul>
+                        
+                    </div>
+                </div>
+            </div>
         </div>
         
     )  
