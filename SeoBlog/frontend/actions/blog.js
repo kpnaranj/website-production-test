a6329e7cbb534cc55d7d335b837ad0552dd62d24
Kevin Narsh <kpnaranj@uwaterloo.ca>
2020-10-21 23:26:39 -0400

    Creation of single page blogs

--- SeoBlog/frontend/actions/blog.js
+++ SeoBlog/frontend/actions/blog.js
@@ -37,3 +37,15 @@ export const listBlogsWithCategoriesAndTags =(skip, limit)=>{
         console.log(err);
     })
 };
+
+export const singleBlog = slug =>{
+    return fetch(`${API}/blog/${slug}`, {
+        method:'GET',
+    }).
+    then(response=>{
+        return response.json();
+    }).
+    catch(err=>{
+        console.log(err);
+    })
+}
